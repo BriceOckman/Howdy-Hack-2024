ebcacfab0b22b95dd45469c6fd2d3a1fd714f71f
Isaac Chacko <isaac.chacko05@gmail.com>
2024-09-28 14:19:17 -0500

    add main

---
+++ main.py
@@ -0,0 +1,75 @@
+
+import cv2
+
+def detect_faces_eyes_from_frame(frame):
+    
+    '''
+    input:
+        frame: image
+    output:
+        int, number of faces detected
+    '''
+    
+    # return variable
+    count = 0
+
+    # Load Haar cascades
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+    # Read an image or video frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Loop over detected faces
+    for (x, y, w, h) in faces:
+        # Draw rectangle around face
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # Focus on the face region
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+        # Detect eyes within the face region
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        # Check if two eyes are detected
+        if len(eyes) == 2:
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            
+            count += 1  # increase count for a successful face+2eyes detection
+    
+    return count
+
+def get_frames(filename):
+    
+    '''
+    input:
+        filename: video filename
+    output:
+        [Image?, ...] list of Image objects (image class unknown)
+    '''
+    
+    video = cv2.videocapture(filename)
+    success, image = video.read()  # read initial frame
+    while success:  # while frame reading is successful
+        yield image
+        success, image = video.read() 
+
+def detect_faces(filename):
+    
+    '''
+    input:
+        filename: video filename
+    output:
+        [int, int, ...] list of faces detected/frame
+    '''
+    
+    return [detect_faces_eyes_from_frame(frame) in get_frames(filename)]
+
+def main():
+    img = cv2.imread('image.jpg')
+    print(detect_faces_eyes_from_frame(img))
